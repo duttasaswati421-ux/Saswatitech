9330040d4d2c72261a7c30567b8dd00ca3605f72
duttasaswati421-ux <duttasaswati421@gmail.com>
2025-09-09 20:11:38 +0530

    Add files via upload

---
+++ do while.cpp
@@ -0,0 +1,16 @@
+//custom countdown using while
+#include<iostream>
+using namespace std;
+int main(){
+	long n;
+	do{
+		cout<<"Enter number (0 to end):";
+		cin>>n;
+		cout<<"you entered:"<<n<<"\n";
+	}while(n!=0);
+	return 0;
+		
+	
+	
+}
+	
